807c04353f0a492bb5d1212089261df592043afa
dhyocarriel <dhyovana.carriel@escola.pr.gov.br>
2024-11-19 00:22:05 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var titulo = document.querySelector('h1');
+titulo.textContent = 'Dhyovana Nutricionista';
+var pacientes = document.querySelectorAll('.Pacientes cadastrados')
+
+var paciente = document.querySelector("h2");
